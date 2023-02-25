b271d8f4b48258684453b3092e67b88cb7a75cd3
Kelvin <ngotho.kelvin@yahoo.com>
2023-02-25 14:26:11 +0300

    Add cards

---
+++ src/components/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { MdMic, MdSettings } from 'react-icons/md';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = (props) => {
+    const { title, left } = props;
+    if (title !== 'All Countries Covid-19 cases') {
+        return (
+            <div className="d-flex justify-content-between align-items-center navbar">
+                <NavLink to="/">{left}</NavLink>
+                <p>{title}</p>
+                <div className="fs-4">
+                    <MdMic className="me-3" />
+                    <MdSettings />
+                </div>
+            </div>
+        );
+    }
+    return (
+        <div className="d-flex justify-content-between align-items-center navbar">
+            <p>{left}</p>
+            <p>{title}</p>
+            <div className="fs-4">
+                <MdMic className="me-3" />
+                <MdSettings />
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
