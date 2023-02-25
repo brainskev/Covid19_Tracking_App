81c1217ce3458349931b8546fb347a6abb6827ac
Kelvin <ngotho.kelvin@yahoo.com>
2023-02-25 22:20:14 +0300

    Update details.js

--- src/pages/Details.js
+++ src/pages/Details.js
@@ -1,99 +1,60 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import { FaLessThan } from 'react-icons/fa';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Navbar from '../components/Navbar';
-import TotalCard from '../components/TotalCard';
+import React, { useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { loadCountries } from '../redux/countrySlice';
-import covid from '../assets/covid.svg';
+import Navbar from '../components/Navbar';
+import CountryCard from '../components/Card';
+import TotalCard from '../components/TotalCard';
+import world from '../assets/world.svg';
 
-const Details = () => {
-  const country = useSelector((state) => state.countries);
-  const { name } = useParams();
-
+const Home = () => {
+  const [myin, setMyin] = useState('');
+  const countries = useSelector((state) => state.countries);
   const dispatch = useDispatch();
+  const ghs = new RegExp(myin, 'i');
+
+  let total = 0;
+  if (countries) {
+    countries.countries?.Countries?.forEach((element) => {
+      total += element.TotalConfirmed;
+    });
+  }
+
   useEffect(() => {
-    dispatch(loadCountries(name));
-  }, [dispatch, name]);
+    dispatch(loadCountries());
+  }, [dispatch]);
 
   return (
     <div>
-      {country
-        && country.countries?.Countries?.filter(
-          (item) => item.Country === name,
-        ).map((country) => (
-          <div key={country.ID}>
-            <Navbar
-              className="nav"
-              title={country.Country}
-              left={<FaLessThan />}
-            />
-            <div className="d-flex align-items-center justify-content-evenly px-2 py-3 main-card">
-              <img src={covid} alt="covid virus" className="map w-25 h-25" />
-              <TotalCard
-                name={country.Country}
-                total={country.TotalConfirmed}
+      <Navbar title="All Countries Covid-19 cases" left="2022" />
+      <div>
+        <div className="d-flex align-items-center justify-content-evenly px-2 py-3 main-card">
+          <img src={world} alt="world map" className="map w-25 h-25" />
+          <TotalCard name="COUNTRIES" total={total} className="fs-2" />
+        </div>
+        <div className="d-flex align-items-center justify-content-around filter">
+          <p className="stats">STATS BY COUNTRY</p>
+          <input
+            type="text"
+            value={myin}
+            onChange={(e) => setMyin(e.target.value.trim())}
+            placeholder="My Search"
+          />
+        </div>
+        <div className="d-flex flex-wrap">
+          {countries
+            && countries.countries?.Countries?.filter(
+              (item) => ghs.test(item.Country) === true,
+            ).map((country) => (
+              <CountryCard
+                key={country.ID}
+                country={country.Country}
+                totalCases={country.TotalConfirmed}
               />
-            </div>
-            <div className="d-flex align-items-center justify-content-around filter">
-              <p className="stats">COUNTRY BREAKDOWN</p>
-            </div>
-            <ListGroup as="ul" className="text-light">
-              <ListGroup.Item
-                as="li"
-                className="d-flex justify-content-between align-items-center"
-              >
-                <h4>Country Code:</h4>
-                <p>{country.CountryCode}</p>
-              </ListGroup.Item>
-              <ListGroup.Item
-                as="li"
-                className="d-flex justify-content-between align-items-center"
-              >
-                <h4>New Confirmed:</h4>
-                <p>{country.NewConfirmed}</p>
-              </ListGroup.Item>
-              <ListGroup.Item
-                as="li"
-                className="d-flex justify-content-between align-items-center"
-              >
-                <h4>New Deaths:</h4>
-                <p>{country.NewDeaths}</p>
-              </ListGroup.Item>
-              <ListGroup.Item
-                as="li"
-                className="d-flex justify-content-between align-items-center"
-              >
-                <h4>Total Deaths:</h4>
-                <p>{country.TotalDeaths}</p>
-              </ListGroup.Item>
-              <ListGroup.Item
-                as="li"
-                className="d-flex justify-content-between align-items-center"
-              >
-                <h4>New Recovered:</h4>
-                <p>{country.NewRecovered}</p>
-              </ListGroup.Item>
-              <ListGroup.Item
-                as="li"
-                className="d-flex justify-content-between align-items-center"
-              >
-                <h4>Total Recovered:</h4>
-                <p>{country.TotalRecovered}</p>
-              </ListGroup.Item>
-              <ListGroup.Item
-                as="li"
-                className="d-flex justify-content-between align-items-center"
-              >
-                <h4>Date:</h4>
-                <p>{country.Date}</p>
-              </ListGroup.Item>
-            </ListGroup>
-          </div>
-        ))}
+            ))}
+        </div>
+      </div>
     </div>
   );
 };
 
-export default Details;
+export default Home;
